40359e0bd17ba0e277ac971207c756fbdd847795
slavo3dev <slavo@slavo3.com>
2021-11-27 15:27:39 -0500

    adding style to input button

--- src/features/focus/index.js
+++ src/features/focus/index.js
@@ -10,23 +10,25 @@ export function Focus() {
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}> What habit are you willing to practice?</Text>
-        <View styles={styles.inputContainer}>
-          <TextInput />
-          <RoundedBtn title={"+"} />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} />
+        <View style={style.btnContainer}>
+          <RoundedBtn title={"+"} size={50}  />
         </View>
       </View>
     </View>
-  );
-}
+  )}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    borderWidth: 5,
+    borderColor: 'black',
   },
   titleContainer: {
-    flex: 0.5,
-    padding: 16,
-    justifyContent: 'center'
+    flex: 0.1,
+    justifyContent: 'center',
   },
   title: {
     color: "#fff",
@@ -34,6 +36,15 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   inputContainer: {
-    paddingTop: 18
+    flex: 0.1,
+    borderWidth: 5,
+    borderColor: 'red',
+    flexDirection: "row"
+  },
+  textInput: {
+    flex: 1
+  },
+  btnContainer: {
+    justifyContent: 'center'
   }
 });
